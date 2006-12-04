6ef7f974ec888be1a5a97c9c8ce076269ae49075
sourcewalker <sourcewalker@6f7f4c55-aa21-0410-9323-47b3203a618a>
2006-12-04 17:24:52 +0000

    WebAPI::Delicious class that allows you to get posts/tags from del.icio.us easily is added.

--- management/update/update.rb
+++ management/update/update.rb
@@ -1,5 +1,17 @@
 #! /usr/bin/ruby
 
 require 'webapi/delicious'
+require 'nkf'
 
-print "abc\n"
+api = WebAPI::Delicious.new('hokousya')
+posts = api.posts(3, 'google-tips')
+
+out = ''
+posts.each do |post|
+  out << "title : #{post.title}\n"
+  out << "url   : #{post.url}\n"
+  out << "note  : #{post.note}\n"
+  out << "tags  : #{post.tags.join(',')}\n"
+end
+
+print NKF.nkf('-s', out)
