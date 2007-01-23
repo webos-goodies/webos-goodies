96c98786deb1ea0948d23352e2413d8d6ad362e7
sourcewalker <sourcewalker@6f7f4c55-aa21-0410-9323-47b3203a618a>
2007-01-23 17:22:24 +0000

    Became to use REST API to obtain posts since the order of posts in json feed is broken. And the page template was also modified.

--- management/update/update.rb
+++ management/update/update.rb
@@ -48,8 +48,8 @@ class Update
     end
 
     def list_articles(tag, max = nil, reverse = true)
-      posts = max ? @articles[tag][0, max] : @articles[tag].clone
-      posts.reverse! if posts.size >= 2 && !reverse
+      posts = max ? @articles[tag][0, max] : @articles[tag]
+      posts = posts.reverse if posts.size >= 2 && !reverse
       text = ''
       posts.each do |post|
         url     = post.url
@@ -88,17 +88,25 @@ class Update
   end
 
   def fetch_articles()
-    WebAPI::Delicious.new(*DELICIOUS).get_bundles().each do |bundle|
+    del = WebAPI::Delicious.new(*DELICIOUS)
+    print "Fetching tags...\n"
+    del.get_bundles().each do |bundle|
       bundle.tags.each do |tag|
         raise "Two or more bundles have #{tag}." if @bundles.has_key?(tag)
         @bundles[tag] = bundle
       end
     end
-    del = WebAPI::Delicious.new(DELICIOUS[0])
+    print "Fetching posts...\n"
+    all_posts = del.get_posts()
     @bundles.each_key do |tag|
-      print "Fetching #{tag}...\n"
-      @articles[tag] = del.get_posts('tags' => [tag])
+      @articles[tag] = all_posts.collect do |post|
+        post.tags.include?(tag) ? post : nil
+      end.compact
     end
+    # @bundles.each_key do |tag|
+    #   print "Fetching #{tag}...\n"
+    #   @articles[tag] = del.get_posts('tags' => [tag])
+    # end
   end
 
   def build_files()
@@ -143,10 +151,12 @@ class Update
     @new_dirs.sort! { |a, b| a<=>b }
     @new_dirs.each do |dir|
       unless File.exist?("#{@old_path}/#{dir}")
+        print "mkdir #{@old_path}/#{dir}...\n"
         @ftp.mkdir("#{@remote_path}/#{dir}")
       end
     end
     @new_files.each_key do |fname|
+      print "send #{@current_path}/#{fname}...\n"
       @ftp.putbinaryfile("#{@current_path}/#{fname}",
                          "#{@remote_path}/#{fname}")
     end
