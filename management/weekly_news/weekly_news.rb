366cec6297a3ee7a320f9761d3056d7e6645696d
sourcewalker <sourcewalker@6f7f4c55-aa21-0410-9323-47b3203a618a>
2008-08-02 14:21:48 +0000

    Fixed bugs in weekly_news.rb.

--- management/weekly_news/weekly_news.rb
+++ management/weekly_news/weekly_news.rb
@@ -2,8 +2,16 @@
 
 require 'webapi/google/ajaxfeeds'
 
+KCODE    = 'UTF8'
 FEED_URL = 'http://myweb.yahoo.com/mywebrss/user/WxOWnIrYtZTWpQf1ec3vYmiA/tag/pending/urls.xml'
 
-WebAPI::Google::AjaxFeeds.load(FEED_URL).each do |entry|
-  p entry.title
+WebAPI::Google::AjaxFeeds.load(FEED_URL, 'num' => -1).each do |entry|
+  print <<EOS
+
+**#{ entry.title }
+
+#{ entry.link }
+
+#{ entry.content }
+EOS
 end
