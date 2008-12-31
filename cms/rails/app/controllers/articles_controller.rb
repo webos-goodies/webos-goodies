086c711310db4d9d2e89da20a3e7acd11b5b8718
sourcewalker <sourcewalker@6f7f4c55-aa21-0410-9323-47b3203a618a>
2008-12-31 19:24:31 +0000

    Implemented preview of an article.

--- cms/rails/app/controllers/articles_controller.rb
+++ cms/rails/app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
 
+  SITE_URL         = 'http://webos-goodies.jp/'
+  RSS_URL          = 'http://webos-goodies.jp/index.rdf'
+  ATOM_URL         = 'http://webos-goodies.jp/atom.xml'
+  SITE_TITLE       = 'WebOS Goodies'
+  SITE_DESCRIPTION = 'Gentoo Linux で個人サーバーから名称変更しました。今後ともよろしくお願いします！'
+
   def index
-    @articles = Article.find(:all)
+    respond_to do |type|
+      type.html { @articles = Article.find(:all) }
+    end
   end
 
   def show
@@ -18,7 +26,11 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params[:article])
-    @article.save!
+    if @article.published
+      publish(@article)
+    else
+      @article.save!
+    end
     redirect_to article_path(@article.id)
   rescue ActiveRecord::RecordInvalid
     render :action => 'new'
@@ -27,7 +39,11 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.attributes = params[:article]
-    @article.save!
+    if @article.published
+      publish(@article)
+    else
+      @article.save!
+    end
     redirect_to article_path(@article.id)
   rescue ActiveRecord::RecordInvalid
     render :action => 'edit'
@@ -36,12 +52,25 @@ class ArticlesController < ApplicationController
   def preview
     article = Article.find(params[:id])
     parser  = Parser::Base.find(article.parser).new
-    @page_name     = article.page_name
-    @title         = article.title
-    @meta          = article.meta
-    @publish_date  = article.publish_date || DateTime.now
-    @body1, @body2 = parser.parse(article.body1, article.body2)
+    @site_url         = SITE_URL
+    @rss_url          = RSS_URL
+    @atom_url         = ATOM_URL
+    @site_title       = SITE_TITLE
+    @site_description = SITE_DESCRIPTION
+    @article_url      = SITE_URL + 'archives/' + ERB::Util.url_encode(article.page_name) + '.html'
+    @article_id       = article.page_name
+    @article_title    = article.title
+    @article_meta     = article.meta
+    @article_date     = article.publish_date || DateTime.now
+    @article_body1, @article_body2 = parser.parse(article.body1, article.body2)
     render :layout => false
+  end
+
+  private
+
+  def publish(article)
+    article.publish_date ||= Time.now
+    article.save!
   end
 
 end
