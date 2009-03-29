4999feb7cf0e26f973863de493103c284bb0516f
sourcewalker <sourcewalker@6f7f4c55-aa21-0410-9323-47b3203a618a>
2009-03-29 15:38:49 +0000

    Create friendconnect_star branch.

--- products/jslib/draggable_grid/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-yuicompressor --charset UTF-8 draggable_grid-debug.js > draggable_grid.js
--- products/jslib/dragresize/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-yuicompressor --charset UTF-8 dragresize-debug.js | jsjuicer -smo > dragresize.js
