04746cd641c74f998c4e2241c96ea1ef560a4a6f
sourcewalker <sourcewalker@6f7f4c55-aa21-0410-9323-47b3203a618a>
2009-06-28 13:17:39 +0000

    Added a new OpenGL on iPhone sample.

---
+++ blog/articles/getting_started_with_opengl_on_iphone/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iphone_gl
+//
+//  Created by 伊藤 千光 on 09/06/28.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
