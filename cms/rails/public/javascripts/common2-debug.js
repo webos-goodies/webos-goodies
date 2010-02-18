90c4cd0bc906f2c0787b9e347f8407fc263442e7
sourcewalker <sourcewalker@6f7f4c55-aa21-0410-9323-47b3203a618a>
2010-02-18 15:20:40 +0000

    Added recent buzz conversations into the sidebar.

--- cms/rails/public/javascripts/common2-debug.js
+++ cms/rails/public/javascripts/common2-debug.js
@@ -1,6 +1,8 @@
 goog.require('goog.dom');
 goog.require('goog.string');
 goog.require('goog.array');
+goog.require('goog.date.relative');
+goog.require('goog.i18n.DateTimeFormat');
 goog.require('wg.FeedReader');
 goog.require('wg.SpreadsheetReader');
 
@@ -17,7 +19,11 @@ if(typeof(history['navigationMode']) != 'undifined') {
 // Create namespace for template code.
 //----------------------------------------------------------
 
-var template = new Object;
+var template                 = {};
+window['template']           = template;
+template['FeedRenderer']     = wg.FeedRenderer;
+wg.FeedRenderer['callback_'] = wg.FeedRenderer.callback_;
+wg.FeedRenderer.prototype.callbackName = 'template.FeedRenderer.callback_';
 
 
 //----------------------------------------------------------
@@ -70,13 +76,21 @@ template.addWindowEvent('ready', function() {
 //----------------------------------------------------------
 
 template['generateRelatedLinks'] = function(json, root) {
+  var docIdMatch = /(\w+)\.html$/;
+  var docId = '';
+  if(window.location.href.match(docIdMatch))
+    docId = RegExp.$1;
+
   var html = [];
   goog.array.forEach(goog.array.slice(json['p'], 0, 5), function(entry, index) {
-	var template = '<li><a href="%u%">%t%</a></li>';
+	var template = (
+	  entry['u'].match(docIdMatch) && RegExp.$1 == docId ?
+		'<li>%t%</li>' : '<li><a href="%u%">%t%</a></li>');
 	html[index] = template.replace(/%([^%]*)%/g, function(match, label) {
 	  return goog.string.htmlEscape(entry[label] || '');
 	});
   });
+
   var catURL = goog.string.htmlEscape(root + 'categories/' + json['t'] + '.html');
   html = goog.array.concat(
 	['<h1><a href="', catURL, '">', json['c'], '</a></h1><ul>'],
@@ -85,22 +99,6 @@ template['generateRelatedLinks'] = function(json, root) {
   html = goog.string.buildString.apply(goog.string.buildString, html)
   return goog.dom.htmlToDocumentFragment(html);
 }
-
-
-//----------------------------------------------------------
-// rendering recent articles
-//----------------------------------------------------------
-
-template['FeedRenderer']     = wg.FeedRenderer;
-wg.FeedRenderer['callback_'] = wg.FeedRenderer.callback_;
-wg.FeedRenderer.prototype.callbackName = 'template.FeedRenderer.callback_';
-template.addWindowEvent('ready', function() {
-  var renderer = new wg.FeedRenderer({
-	'key':'ABQIAAAADFolcpMzeDEXDBR65zomPRSdobuQ8nl73Zh0G-Y7QnxRnfXdORRvX5O5---NvrXXjsKrVcjvSimLkw'
-  });
-  renderer.setTemplate('<div class="sidebody"><a href="%link%">%title%</a></div>');
-  renderer.render('http://webos-goodies.jp/atom.xml', 'tpl_recent_articles', 8);
-});
 
 
 //----------------------------------------------------------
@@ -181,4 +179,34 @@ template['renderTrackbacks'] = function(pageId) {
   });
 };
 
-window['template'] = template;
+
+//----------------------------------------------------------
+// show recent articles and buzz.
+//----------------------------------------------------------
+
+var buzzFormatter = {
+  'publishedDate': function(value, entry) {
+	var date   = new Date(value);
+	var format = new goog.i18n.DateTimeFormat(goog.i18n.DateTimeFormat.Format.MEDIUM_DATETIME);
+	var str    = (goog.date.relative.formatPast(date.getTime()) || format.format(date));
+	return str;
+  }
+};
+
+template.addWindowEvent('ready', function() {
+  var renderer = new wg.FeedRenderer({
+	'key':'ABQIAAAADFolcpMzeDEXDBR65zomPRSdobuQ8nl73Zh0G-Y7QnxRnfXdORRvX5O5---NvrXXjsKrVcjvSimLkw'
+  });
+
+  // recent articles
+  renderer.setTemplate('<a class="sidebody" href="%link%">%title%</a>');
+  renderer.render('http://webos-goodies.jp/atom.xml', 'tpl_recent_articles', 8);
+
+  // buzz
+  renderer.setTemplate(goog.string.buildString(
+	'<a class="sidebody" href="%link%" target="_blank">%contentSnippet%<br>',
+	'<span class="tpl-buzz-date">%publishedDate%</span></a>'));
+  renderer.setFormatter(buzzFormatter);
+  renderer.render('http://buzz.googleapis.com/feeds/113438044941105226764/public/posted',
+				  'tpl_buzz', 16);
+});
