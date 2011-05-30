f1840017357d730abdad28a43a490a6f79ef945c
sourcewalker <sourcewalker@6f7f4c55-aa21-0410-9323-47b3203a618a>
2011-05-30 16:30:37 +0000

    Removed namespace wrapper.

--- products/jslib/dragresize/build.sh
+++ products/jslib/dragresize/build.sh
@@ -6,4 +6,4 @@ DEST_FILE=dragresize.js
 
 #yuicompressor --charset UTF-8 dragresize-debug.js | jsjuicer -smo > dragresize.js
 
-java -jar $CLOSURE_COMPIER --js=$SRC_FILE --js_output_file=$DEST_FILE --output_wrapper='(function(){%output%})();' --compilation_level=ADVANCED_OPTIMIZATIONS
+java -jar $CLOSURE_COMPIER --js=$SRC_FILE --js_output_file=$DEST_FILE --compilation_level=ADVANCED_OPTIMIZATIONS
