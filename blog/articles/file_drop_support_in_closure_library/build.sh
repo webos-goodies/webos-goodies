48ae155700d916770fc5ad2d704f20beb3d2a632
sourcewalker <sourcewalker@gmail.com>
2013-03-10 03:38:40 +0000

    follows recent file api spec.

--- blog/articles/file_drop_support_in_closure_library/build.sh
+++ blog/articles/file_drop_support_in_closure_library/build.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
-python closure-library/closure/bin/build/closurebuilder.py --root=. -n RequiredScripts -o compiled --output_file=script.js -c compiler.jar -f "--compilation_level=SIMPLE_OPTIMIZATIONS" -f "--define=goog.DEBUG=false"
+CLOSURE_LIBRARY=../../../closure-library
+
+python $CLOSURE_LIBRARY/closure/bin/build/closurebuilder.py --root=$CLOSURE_LIBRARY --root=. -n RequiredScripts -o compiled --output_file=closure-library/closure/goog/base.js -c compiler.jar -f "--compilation_level=SIMPLE_OPTIMIZATIONS" -f "--define=goog.DEBUG=false"
